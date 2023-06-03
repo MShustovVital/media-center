92429b456a0348ed0a5183daebcd61d3e1ca9f40
Mark Shustov <mshustov@marfa-tech.com>
2023-06-03 16:59:26 +0300

    add dump

---
+++ dump/dump.sql
@@ -0,0 +1,73 @@
+CREATE TABLE film_genres
+(
+    id  SERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE film_types
+(
+    id  SERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE roles
+(
+    id  SERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE countries
+(
+    id  SERIAL PRIMARY KEY,
+    name VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE films
+(
+    id  SERIAL PRIMARY KEY,
+    name          VARCHAR(128)   NOT NULL,
+    releaseDate   DATE           NOT NULL,
+    film_type_id  BIGINT         NOT NULL,
+    film_genre_id BIGINT         NOT NULL,
+    country_id    BIGINT         NOT NULL,
+    description   VARCHAR(255)   NULL,
+    duration      INT         NOT NULL,
+    rating        DECIMAL(10, 2) NOT NULL,
+    imageUrl      VARCHAR(255)   NULL,
+    FOREIGN KEY (film_type_id) REFERENCES film_types (id),
+    FOREIGN KEY (film_genre_id) REFERENCES film_genres (id),
+    FOREIGN KEY (country_id) REFERENCES countries (id),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE people
+(
+    id  SERIAL PRIMARY KEY,
+    name          VARCHAR(64)  NOT NULL,
+    surname       VARCHAR(64)  NOT NULL,
+    full_name     VARCHAR(255) GENERATED ALWAYS AS (name || ' ' || surname) STORED,
+    date_of_birth DATE         NOT NULL,
+    description   VARCHAR(255) NULL,
+    imageUrl      VARCHAR(255) NULL
+);
+
+CREATE TABLE people_film_roles
+(
+    role_id   BIGINT NOT NULL,
+    people_id BIGINT NOT NULL,
+    film_id   BIGINT NOT NULL,
+
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (people_id) REFERENCES people (id),
+    FOREIGN KEY (film_id) REFERENCES films (id),
+    UNIQUE (role_id, people_id, film_id)
+);
+
+CREATE INDEX idx_name ON films (name);
+CREATE INDEX idx_rating ON films (rating);
+CREATE INDEX idx_film_type_id ON films (film_type_id);
+CREATE INDEX idx_film_genre_id ON films (film_genre_id);
+CREATE INDEX idx_country_id ON films (country_id);
+
+CREATE INDEX idx_full_name on people (full_name);
